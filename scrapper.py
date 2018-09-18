83ab980cbd4bf78ea745fd26fecfe5af4920ae21
supritkumar shah <suprit7@gmail.com>
2018-09-17 22:35:43 -0400

    works, but requires some enhancemnets

--- scrapper.py
+++ scrapper.py
@@ -1,18 +1,3 @@
-# # import libraries
-# import urllib2
-# from bs4 import BeautifulSoup
-# # specify the url
-# hindi_movie_page = 'https://einthusan.tv/movie/results/?find=Year&lang=hindi&page=1&year=2018'
-# # query the website and return the html to the variable 'page'
-# page = urllib2.urlopen(hindi_movie_page)
-# # parse the html using beautiful soup and store in variable 'soup'
-# soup = BeautifulSoup(page, 'html.parser')
-# # Take out the <div> of name and get its value
-# for a in soup.find_all('a', href=True):
-#     print "Found the URL:", a['href']
-
-
-
 # from bs4 import BeautifulSoup
 # import urllib2
 # import re
@@ -30,22 +15,45 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-base_enthu_url = 'https://einthusan.tv'
-title_urls = {} #dictionary
-# for x in range(1,6):
-url = 'https://einthusan.tv/movie/results/?find=Year&lang=hindi&page=1&year=2018'
-resp = requests.get(url)
-soup = BeautifulSoup(resp.text, 'lxml')
-for div in soup.find_all('div', {"class": "block2"}):
-    a = div.find('a')
-    h3 = a.find('h3')
-    title_urls[h3.text] = base_enthu_url+a.attrs['href']
-    # urls.append(base_enthu_url+a.attrs['href'])
+#base url
+url = 'https://einthusan.tv/movie/results/?find=Year&lang=hindi&page=2&year=2017'
 
-print(title_urls)
+#base url to make a complete enthu url of a movie link 
+base_enthu_url = 'https://einthusan.tv'
+
+#dictionary object to hold result of scraping
+title_and_urls = {} #dictionary
+
+#method take an argument a url and returns number of page in a given year
+def getNumberOfPages(url):
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    for div in soup.find_all('div', {"class": "results-info"}):
+        p = div.find("p").text
+        lenOfPageString = len(p)
+        if lenOfPageString == 25:
+            num = p[9:11] 
+        else:
+            num = p[9:12]
+        return num.strip()
+
+totalNumberOfPages = getNumberOfPages(url)
+
+
+for x in range(1,int(totalNumberOfPages)+1):
+    url_pages = 'https://einthusan.tv/movie/results/?find=Year&lang=hindi&page=' +str(x)+'&year=2017'
+    resp = requests.get(url_pages, timeout=10)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    for div in soup.find_all('div', {"class": "block2"}):
+        a = div.find('a')
+        h3 = a.find('h3')
+        print(h3,url_pages)
+        title_and_urls[h3.text] = base_enthu_url+a.attrs['href']
+
+print(title_and_urls)
 
 
 with open('dict.csv', 'wb') as csv_file:
     writer = csv.writer(csv_file)
-    for key, value in title_urls.items():
+    for key, value in title_and_urls.items():
        writer.writerow([key, value])
